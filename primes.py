b884e474b8d365a3ef7fe0ac25a22fcf12c24723
haiwei-luo <haiwei8809@gmail.com>
2023-10-08 01:18:32 +0100

    Complete the task of getting the first n prime nymbers, with code commented out

--- primes.py
+++ primes.py
@@ -1,6 +1,27 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def checkPrime(num):
+    isPrime = True
+    counter = 2
+    if num == 1:
+        isPrime = False
+    else:
+        while counter < num:
+            if num % counter == 0:
+                isPrime = False
+            counter = counter + 1
+    return isPrime 
+
 def primes(number_of_primes):
     list = []
+    currentNum = 1;
+    while len(list) < number_of_primes:
+        result = checkPrime(currentNum)
+        if result == True:
+            list.append(currentNum)
+        currentNum = currentNum + 1
     return list
+
+print(checkPrime(7))
+print(primes(10))
